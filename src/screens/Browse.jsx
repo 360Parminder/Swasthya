543d882315690c1434af9130e3b1ecfb5589a11b
360Parminder <yesask8@gmail.com>
2024-06-28 18:30:31 +0530

    Added Categories Section Design

--- src/screens/Browse.jsx
+++ src/screens/Browse.jsx
@@ -4,11 +4,24 @@ import React from 'react'
 const Browse = () => {
   return (
     <View>
-     
+     <Text>Categories</Text>
     </View>
   )
 }
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        },
+    title:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+        textAlign: 'center',
+        marginVertical: 10,
+        
+    },  
 })
 export default Browse

0c2492de06f23b848fad0d1e039c3d66b16ab0c3
360Parminder <yesask8@gmail.com>
2024-08-05 23:13:47 +0530

    added Home Screen Widget feature in Android

---
+++ android/app/src/main/java/com/Swasthya/WidgetProvider.kt
@@ -0,0 +1,38 @@
+package com.Swasthya
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class WidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    companion object {
+        fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val views = RemoteViews(context.packageName, R.layout.widget_layout)
+
+            // Set up the intent that starts the MainActivity when clicked
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+            views.setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+
+            // Update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}

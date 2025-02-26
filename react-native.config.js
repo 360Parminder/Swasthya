20cb36658642281567e10753dd52c90b29873ab7
360Parminder <yesask8@gmail.com>
2025-02-26 23:18:04 +0530

    Update react-native.config.js to configure font assets

--- react-native.config.js
+++ react-native.config.js
@@ -1,20 +1,3 @@
-// module.exports = {
-//   dependencies: {
-//     'react-native-vector-icons': {
-//       platforms: {
-//         ios: null, // Disable auto-linking for iOS if necessary
-//       },
-//     },
-//     'react-native-ionicons': {
-//       platforms: {
-//         ios: null, // Disable auto-linking for iOS if necessary
-//         android: null, // Disable auto-linking for Android if necessary
-//       },
-//     },
-//   },
-// };
-
-
 module.exports = {
   assets: ['./src/assets/fonts'],
   dependencies: {
